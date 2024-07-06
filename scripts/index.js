0b0f8d41caf2c70ba45acd4f8a27677de2c0ee90
Ivan Zyatkov <Ziozlat@yandex.ru>
2024-07-06 16:43:01 +0500

    First commit 5pr

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,28 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function addCard (item, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__image').src = item.link;
+  cardElement.querySelector('.card__image').alt = item.name;
+  cardElement.querySelector('.card__title').textContent = item.name;
+  deleteButton.addEventListener('click', deleteCard);
+
+  return cardElement;
+};
+
+function deleteCard (evt) {
+  cardItem = evt.target.closest('.places__item');
+  cardItem.remove();
+};
+
+initialCards.forEach(function (item) {
+  const card = addCard(item, deleteCard);
+  placesList.append(card);
+});
